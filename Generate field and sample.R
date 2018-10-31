ee251ff977a6da38dabc98b3c9ac64c465cda4a0
emilygsimmonds <39558355+emilygsimmonds@users.noreply.github.com>
2018-10-31 11:45:45 +0100

    Update Generate field and sample.R
    Added some extra comments where I thought I might forget what the code does.

--- Generate field and sample.R
+++ Generate field and sample.R
@@ -1,42 +1,53 @@
 ##generate spatial field from log gaussian cox process
 ##this code is taken from the Simpson tutorial 
 
-#xdim = 100, ydim = 300
 
 library(spatstat) 
-win <- owin(c(0,1), c(0,3))
+# owin creates an object of class "owin" which is an observation window in 2D
+# specify x and y coordinates
+win <- owin(c(0,3), c(0,3))
 
-spatstat.options(npixel=c(100, 300))
+# set number of pixels
+spatstat.options(npixel=300)
 
-beta0 <- 3
+
+beta0 <- 3 # intercept/mu
 
 sigma2x <- 0.2;      kappa <- 2
 
 library(RandomFields) 
 set.seed(1) 
+# below generates a random point pattern (Log-Gaussian Cox process)
+# exponential of this is taken as the intensity of Poisson point process
 lg.s <- rLGCP('matern', beta0, 
               var=sigma2x, scale=1/kappa, nu=1, win=win)
 
-xy <- cbind(lg.s$x, lg.s$y)
+# takes the coordinates of the randomly generated points
+xy <- cbind(lg.s$x, lg.s$y)[,2:1]
 
+# access attribute (Lambda) of lg.s object and create Lam 
 Lam <- attr(lg.s, 'Lambda') 
 summary(as.vector(rf.s <- log(Lam$v)))
+# can convert this to a Poisson point process with below
+ppLam <- rpoispp(Lam)
 
+# plot the intensity and the points (realisations?)
 par(mfrow=c(1,1)) 
 library(fields) 
-image.plot(list(x=Lam$xcol, y=Lam$yrow, z=t(rf.s)), main='log-Lambda', asp=1) 
+image.plot(list(x=Lam$yrow, y=Lam$xcol, z=rf.s), main='log-Lambda', asp=1) 
 points(xy, pch=19)
-
 
 
 ##function splits the surface into a grid of defined size
 source('Generate strata levels Lam.R')
 par(mfrow = c(1,1))
+# creates a dataframe of x and y coordinates, intensity and assigned grid square
 strata1 <- genStrataLam(Lam, strata = 8, rows = 4, cols = 2)
 
 
 ## Create lookup table between strata and probabilities
 ## NOTE this section must be adjusted if number of strata change and/or desired probabilities change
+# lookup is another dataframe of stratum number and associated probability
 lookup <- data.frame(stratum = 1:8, probs = NA)
 
 ## Specify probs for each stratum (note this is effectively a detection probability per point NOT a probability of each cell being sampled i.e. does not sum to 1)
@@ -55,12 +66,14 @@ stratumProbs <- function(strata, probs){
   }
   return(strata)
 }
+# add detection probability per point defined by which grid square it is in
 dat <- stratumProbs(strata1, lookup$probs)
 
 
 par(mfrow=c(1,1))
 plot(dat$y ~ dat$x, pch = 20, col = factor(dat$stratprobs))
-points(xy*100, pch=19, col= "white")
+points(xy*100, pch=19, col= "white") 
+# strata appears to be original scale * 100 not sure why
 
 ###now need to use strata in a different way to previously...sample from the points generated in the first section (xy) according to associated strata prob
 
@@ -74,17 +87,19 @@ plot(dat$y ~ dat$x, pch = 20, col = factor(dat$stratprobs))
 points(pp2, pch=19, col= pp2$stratprobs*10)
 
 #thin points using the stratum-based detection probability
+#reducing also to presence absence here not abundance
 pp2$presence <- rbinom(nrow(pp2),1,pp2$stratprobs)
 
+# make it presence only data
 pp3 <- pp2[pp2$presence == 1,]
 
 
-image.plot(list(x=Lam$xcol, y=Lam$yrow, z=t(rf.s)), main='log-Lambda', asp=1) 
-points(pp3$x/100, pp3$y/100, pch = 20)#note rescale again
+image.plot(list(x=Lam$yrow, y=Lam$xcol, z=rf.s), main='log-Lambda', asp=1) 
+points(pp3$x/100, pp3$y/100, pch = 20)#note rescale again - plotting back on original
 
 #distribution of points is now biased to the lower half of the region. Therefore pp3 could be unstructured data collection
 
-#how to simulate structured?
+#how to simulate structured? 
 
 #basic approach - simulate stratified random set of sampling areas (5*5 points)
 
@@ -93,38 +108,23 @@ dat$sim2 <- dat$sim1 #no extra error added
 
 s1 <- sampleStrata(dat, nsamp = 24, type = "Stratified")
 
-#add neighbourhood
+#add neighbourhood - areas of human occupancy?
 
 s2 <- data.frame()
 for(i in 1:nrow(s1$Stratified)){
   s3 <- dat[dat$x %in% seq(s1$Stratified$x[i]-2, s1$Stratified$x[i]+2) & dat$y %in% seq(s1$Stratified$y[i]-2, s1$Stratified$y[i]+2),]
-s2 <- rbind(s2, s3)
+  s2 <- rbind(s2, s3)
 }
 
 par(mfrow=c(1,1))
-image.plot(list(x=Lam$xcol, y=Lam$yrow, z=t(rf.s)), main='log-Lambda', asp=1) 
+image.plot(list(x=Lam$yrow, y=Lam$xcol, z=rf.s), main='log-Lambda', asp=1) 
 points(s2$x/100, s2$y/100, pch = 20, col = "blue")#note rescale again
+points(xy, pch=20, col= "white")
 
 
-##need to generate new points - this would remove the assumption that the same individuals are observed in both processes. Still assume that the random field is the same
-
-newpoints <- rpoispp(lambda = Lam)
-points(newpoints$y ~ newpoints$x, pch = 20, col = "white")
 
 #see which points are observed 
 
-newpoints_sc <- data.frame(x1 = round(newpoints$x*100), y1 = round(newpoints$y*100))
+dat2 <- merge(pp2, s2, by.x = c("x", "x"), by.y = c("y","y"))
 
-dat2 <- merge(newpoints_sc, s2, by.x = c("x1", "y1"), by.y = c("x","y"))
-
-names(dat2) <- c("x1", "y1", "sim1", "stratum", "stratprobs", "sim2")
-
-head(dat2)
-
-dat2$x_sc <- dat2$x/100
-dat2$y_sc <- dat2$y/100
-
-points(dat2$y_sc ~ dat2$x_sc, pch = 20, col = "red")
-
-#dat2 now holds locations of points observed in structured survey
-
+##need to generate new points?

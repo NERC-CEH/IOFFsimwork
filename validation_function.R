7881201ea6a5cbd389df6a241074cfcd989a6725
Emily Simmonds <emilygs@dhcp-15226.math.ntnu.no>
2018-11-08 16:24:57 +0100

    updated

--- validation_function.R
+++ validation_function.R
@@ -10,7 +10,9 @@
 validation_function <- function(result, resolution, join.stack, model_type = c("unstructured", "structured", "joint"), unstructured_data=NULL,
                              structured_data=NULL, dat1,
                              plot = F, summary_results = F){
-
+  
+if(model_type == "structured"){transformation <- function(x){1-exp(-exp(x))}}else{transformation <- function(x){exp(x)}}
+  
 index.pred.response <- inla.stack.index(join.stack, tag="pred.response")$data
 
 m.prd <- result$summary.fitted.values$mean[index.pred.response]
@@ -19,7 +21,7 @@ sd.prd <- result$summary.fitted.values$sd[index.pred.response]
 # calculate differences
 source('make_truth_grid.R')
 truth_grid <- make_truth_grid(c(10,10), dat1, c(100,300), type='truth') # grid truth and take averaged
-differences <- (exp(m.prd)-mean(exp(m.prd)))-truth_grid # calculate differences
+differences <- (transformation(m.prd)-mean(transformation(m.prd)))-truth_grid # calculate differences
 
 if(plot == T){
 par(mfrow=c(1,4))
@@ -30,9 +32,9 @@ if(is.null(structured_data)==FALSE){points(structured_data[structured_data[,4] %
   points(structured_data[structured_data[,4] %in% 1,2:3], pch=16, col='black')}
 #predicted mean
 image.plot(seq(resolution[1]/2,100,resolution[1]),seq(resolution[2]/2,300,resolution[2]), 
-           matrix(exp(m.prd), ncol=30, nrow=10), col=tim.colors(),xlab='', ylab='',main="Predicted mean intensity",asp=1)
+           matrix(transformation(m.prd), ncol=30, nrow=10), col=tim.colors(),xlab='', ylab='',main="Predicted mean intensity",asp=1)
 image.plot(seq(resolution[1]/2,100,resolution[1]),seq(resolution[2]/2,300,resolution[2]), 
-           matrix(exp(sd.prd), ncol=30, nrow=10), col=tim.colors(),xlab='', ylab='',main="Predicted sd intensity",asp=1)
+           matrix(transformation(sd.prd), ncol=30, nrow=10), col=tim.colors(),xlab='', ylab='',main="Predicted sd intensity",asp=1)
 # relative differences
 image.plot(seq(resolution[1]/2,100,resolution[1]),seq(resolution[2]/2,300,resolution[2]), 
            matrix(differences, ncol=30, nrow=10), col=tim.colors(),xlab='', ylab='',main="Relative differences",asp=1)
@@ -41,11 +43,22 @@ image.plot(seq(resolution[1]/2,100,resolution[1]),seq(resolution[2]/2,300,resolu
 if(summary_results == T){
   RSE_differences <- sqrt(differences^2)
   grid <- make_truth_grid(c(10,10), dat1, c(100,300), type='grid')
+  coefficients <- result$summary.fixed
+  #back transform all of the coefficient values so they are comparable
+  if(length(which(row.names(coefficients) %in% "interceptA")==T)>0){coefficients[row.names(coefficients) %in% "interceptA",] <- 
+    1-exp(-exp(coefficients[row.names(coefficients) %in% "interceptA",]))}
+  if(length(which(row.names(coefficients) %in% "interceptB")==T)>0){coefficients[row.names(coefficients) %in% "interceptB",] <- 
+    exp(coefficients[row.names(coefficients) %in% "interceptB",])}
+  if(length(which(row.names(coefficients) %in% "interceptB")==T)==0){coefficients[row.names(coefficients) %in% "env",] <- 
+    1-exp(-exp(coefficients[row.names(coefficients) %in% "env",]))}else{coefficients[row.names(coefficients) %in% "env",] <- 
+      exp(coefficients[row.names(coefficients) %in% "env",])}
+  
   summary_results = list(data.frame(Model = model_type,
                      RMSE = mean(RSE_differences)),
+               coefficients = coefficients[,c(1,3,5,6)]     
                differences,
-               worst_areas <- unique(grid[which(RSE_differences>(mean(RSE_differences)+sd(RSE_differences)))]),
-               best_areas <- unique(grid[which(RMSE_differences>(mean(RMSE_differences)-sd(RMSE_differences)))])
+               worst_areas = unique(grid[which(RSE_differences>(mean(RSE_differences)+sd(RSE_differences)))]),
+               best_areas = unique(grid[which(RSE_differences<(mean(RSE_differences)-sd(RSE_differences)))])
                )
   names(summary_results) <- c("Proto-table", "All_differences", "Worst_grid_cells", "Best_grid_cells")
   return(summary_results)

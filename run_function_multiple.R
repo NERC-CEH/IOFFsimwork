bcfb913f49dbc433f5b58c1332272a7dd924dbee
emilygsimmonds <emilygsimmonds@gmail.com>
2019-04-30 15:58:20 +0200

    New script to summarise outputs from parallel

--- run_function_multiple.R
+++ run_function_multiple.R
@@ -2,7 +2,8 @@
 
 run_function_multiple <- function(resolution=c(10,10), model_type = c("unstructured", "structured", "joint", "unstructuredcov", "jointcov", "joint2"), unstructured_data=NULL,
                                    structured_data=NULL, dat1,
-                                   plotting = FALSE, summary_results = FALSE, biasfield = NULL){
+                                   plotting = FALSE, summary_results = FALSE, biasfield = NULL,
+                                   nsamp = 150){
 
 source("Functions to generate data and sample.R")
 
@@ -78,5 +79,5 @@ validation_r <- validation_function(result=mod_6[[2]], resolution=c(10,10), join
                                         dat1 = dat1, summary_results=T, absolute = FALSE, dim = dim, plotting = TRUE)
 }
 
-return(list(validation, validation_r))
+return(list(validation_r, nsamp))
 }

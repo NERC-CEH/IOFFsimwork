58611044dcd4b91f89c86729049f8847d65ccb60
emilygsimmonds <emilygsimmonds@gmail.com>
2019-11-21 10:45:12 +0000

    Updated make_truth_grid.R to evaluate at points not averages of grids

--- make_truth_grid.R
+++ make_truth_grid.R
@@ -4,21 +4,11 @@ make_truth_grid <- function(resolution, dat1, dimensions, type=c('truth', 'grid'
   
   minnum <- resolution[1]-1
   
-  grid = matrix(NA, nrow=dimensions[2], ncol=dimensions[1])
-  grid_numbers <- 1:prod(dimensions/resolution)
-  # loop for y values
-  for(j in 1:(dimensions[2]/resolution[2])){
-    index.y <- seq(((j-1)*(dimensions[2]/resolution[2]))+1,(j*dimensions[2]/resolution[2]),1)
-    temp_grid_numbers <- grid_numbers[index.y]
-    row_nos <- seq((j*resolution[1]-minnum),(j*resolution[1]),1)
-    # loop for x values
-    for(i in 1:(dimensions[1]/resolution[1])){
-      index.x <- seq((i*resolution[1]-minnum),(i*resolution[1]),1)
-      grid[row_nos,index.x] <- temp_grid_numbers[i]
-    }
-  }
+  # set up the same as prediction stack
+  grid <- expand.grid(x=seq(resolution[1]/2,max(biasfield$x),resolution[1]), 
+                     y=seq(resolution[2]/2, max(biasfield$y),resolution[2])) 
   
-  # sum average abundance by grid square for truth
+  # extract abundance by point for truth
   output <- rep(NA, length(1:max(grid)))
   if(absolute == TRUE){
     data <- dat1$rf.s
@@ -26,10 +16,14 @@ make_truth_grid <- function(resolution, dat1, dimensions, type=c('truth', 'grid'
   if(absolute == FALSE){
     data <- dat1$rf.s-mean(dat1$rf.s)
   }
-  for(i in 1:max(grid)){
-    marker <- which(grid==i)
-    output[i] <- mean(data[marker])
-  }
-  if(type=='truth'){return(output)}
+  # extract abundance values from the truth
+  grid$abundance <- data[Reduce('cbind', nearest.pixel(
+  grid[,1], grid[,2],
+  im(data)))]
+      
+  image.plot(list(x=dat1$Lam$xcol, y=dat1$Lam$yrow, z=t(data)), main='log-Lambda', asp=1)
+      
+
+  if(type=='truth'){return(grid$abundance)}
   if(type=='grid'){return(grid)}
 }
